65206fc954bd57924df185bf60dae8695be6fde9
Raghu narayana <raghupolisetty5@gmail.com>
2026-06-17 22:39:26 +0530

    Enable future behavior in SQLAlchemy engine

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, future=True)
 
 SessionLocal = sessionmaker(
     autocommit=False,
